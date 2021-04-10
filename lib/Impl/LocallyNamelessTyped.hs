2d885a5d129ecf7e9f5de567bad6f85eead9a159
sweirich <sweirich@seas.upenn.edu>
2021-04-10 16:04:15 -0400

    delaying the LN multisubst\

--- lib/Impl/LocallyNamelessTyped.hs
+++ lib/Impl/LocallyNamelessTyped.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE TypeOperators #-}
 
 -- | Based directly on transliteration of Coq output for Ott Locally Nameless Backend
--- Then with types addded, and multi substitutions
--- And caching openning substitutions at binders
-module Impl.LocallyNamelessV2 where
+-- Then with types addded
+module Impl.LocallyNamelessTyped where
 
 import qualified Control.Monad.State as State
 import qualified Data.IntMap as IM
@@ -27,12 +25,6 @@ import qualified Unsafe.Coerce as Unsafe
 -- 1. Well-typed (slows it down)
 -- lennart: 1.43s
 -- random: 1.8ms
-
--- 2. Well-typed multisubst
-
--- 3. Combo multisubst
--- lennart: 4.10 ms (wow!)
--- random: 1.46 ms (a little slow)
 -------------------------------------------------------------------
 
 -- Index to keep track of bound variable scope
@@ -102,18 +94,16 @@ instance NFData (SNat a) where
 -- This means that we do *not* need to shift as much.
 
 data Bind a k where
-  Bind :: Sub n (S k) -> !(a (Plus n (S k))) -> Bind a k
-
--- Bind :: b -> !(a (S k)) -> Bind a k
+  --Bind :: Sub n (S k) -> !(a (Plus n (S k))) -> Bind a k
+  Bind :: b -> !(a (S k)) -> Bind a k
 
 -- create a binding by "abstracting a variable"
--- doesn't remember any previously opened bindings
 bind :: SNat k -> a (S k) -> Bind a k
 bind k x = Bind (Sub (SS k) VNil) x
 {-# INLINEABLE bind #-}
 
 unbind :: forall k. Bind Exp k -> Exp (S k)
-unbind (Bind ss a) = multi_open_exp_wrt_exp_rec ss a
+unbind (Bind _ss a) = a -- multi_open_exp_wrt_exp_rec ss a
 {-# INLINEABLE unbind #-}
 
 instance (Eq (Exp (S n))) => Eq (Bind Exp n) where
@@ -175,23 +165,13 @@ plus_inj :: forall n k m. Plus n (S k) :~: S m -> Plus n k :~: m
 plus_inj = Unsafe.unsafeCoerce Refl
 
 multi_open_exp_wrt_exp_rec :: forall n k. Sub n k -> Exp (Plus n k) -> Exp k
-multi_open_exp_wrt_exp_rec ss@(Sub (k :: SNat k) (vn :: Vec n)) e =
+multi_open_exp_wrt_exp_rec ss@(Sub k v) e =
   case e of
-    Var_b (i :: Idx (Plus n k)) -> openIdx i k vn
+    Var_b (i :: Idx (Plus n k)) -> openIdx i k v
     Var_f x -> Var_f x
-    Abs
-      ( Bind
-          (Sub (nk1 :: SNat (S (Plus n k))) (vm :: Vec m))
-          -- (ss1 :: Sub m (S (Plus n k)))
-          (b :: Exp (Plus m (S (Plus n k))))
-        ) -> Abs (Bind ss2 b2)
-        where
-          ss2 :: Sub (Plus m n) (S k)
-          ss2 = Sub (SS k) (append vm vn)
-          b2 :: Exp (Plus (Plus m n) (S k))
-          b2 = Unsafe.unsafeCoerce b
-    --  | Refl <- plus_comm @n @k ->
-    --    Abs (Bind k (multi_open_exp_wrt_exp_rec (Sub (SS k) v) b))
+    Abs (Bind ss1 b) --  -> Abs (Bind (ss1 `appendSub` ss0) b)
+      | Refl <- plus_comm @n @k ->
+        Abs (Bind k (multi_open_exp_wrt_exp_rec (Sub (SS k) v) b))
     (App e1 e2) ->
       App
         (multi_open_exp_wrt_exp_rec ss e1)
@@ -220,6 +200,15 @@ openIdx (FS (m :: Idx m0)) (SS (k :: SNat k0)) v
     p5 :: m0 :~: Plus n k0
     p5 = Unsafe.unsafeCoerce Refl
 
+{-
+openIdx _ SZ (SCons =
+openIdx (FS n) (SCons _u ss) = weaken <$> lookupIdx n ss
+openIdx (FS n) SNil = Nothing
+openIdx (FZ (SCons u _) =
+-}
+--open_bind_wrt_bind :: SNat n -> Exp Z -> Bind Exp (S n) -> Bind Exp n
+--open_bind_wrt_bind k1 u (Bind (Sub k1 e) = Bind (open_exp_wrt_exp_rec (SS k) u e)
+
 -- either n is equal to m or greater than m
 compareIdx :: SNat k -> Idx (S k) -> Maybe (Idx k)
 compareIdx SZ FZ = Nothing
@@ -227,23 +216,26 @@ compareIdx (SS m) (FS n) = FS <$> compareIdx m n
 compareIdx SZ (FS m) = Nothing
 compareIdx (SS _) FZ = Just FZ
 
-open :: Bind Exp Z -> Exp Z -> Exp Z
-open
-  ( Bind
-      (Sub (k :: SNat (S Z)) (vs :: Vec n))
-      (e :: Exp (Plus n (S Z)))
-    )
-  u
-    | Refl <- plus_comm @n @Z =
-      f
-    where
-      f = multi_open_exp_wrt_exp_rec ss e'
-      ss :: Sub (S n) Z
-      ss = Sub SZ (VCons u vs)
-      e' :: Exp (Plus (S n) Z)
-      e' = Unsafe.unsafeCoerce e
+{-
+open_exp_wrt_exp_rec :: forall n. SNat n -> Exp Z -> Exp (S n) -> Exp n
+open_exp_wrt_exp_rec k u e =
+  case e of
+    (Var_b (n :: Idx (S n))) ->
+      case compareIdx k n of
+        Just i -> Var_b i
+        Nothing -> weaken u
+    (Var_f x) -> Var_f x
+    (Abs b) -> Abs (open_bind_wrt_bind k u b)
+    (App e1 e2) ->
+      App
+        (open_exp_wrt_exp_rec k u e1)
+        (open_exp_wrt_exp_rec k u e2)
+-}
 
------------------------------------------------------------------
+open :: Bind Exp Z -> Exp Z -> Exp Z
+open (Bind _ss e) u = f
+  where
+    f = multi_open_exp_wrt_exp_rec (Sub SZ (VCons u VNil)) e
 
 -- if n2 is greater than n1 increment it. Otherwise just return it.
 cmpIdx :: Idx (S n) -> Idx n -> Idx (S n)
@@ -257,6 +249,16 @@ cmpIdx n1 n2 =
 -- Create a new "bound index" from a free variable
 -- The index starts at FZ and comes from a larger scope
 -- All variables that are less than the new index must be incremented.
+
+{-
+close_subst_wrt_exp :: Idx (S n) -> IdInt -> Sub Exp m n -> Maybe (Sub Exp m (S n))
+close_subst_wrt_exp FZ x1 (Cons (Var_f x2) (Inc SZ))
+  | x1 == x2 = Just $ Inc SZ
+close_subst_wrt_exp (FS m) x1 (Cons u ss) =
+  do
+    ss' <- close_subst_wrt_exp m x1 ss
+    return $ Cons (close_exp_wrt_exp_rec (FS m) x1 u) ss'
+-}
 
 close_exp_wrt_exp_rec :: SNat n -> Idx (S n) -> IdInt -> Exp n -> Exp (S n)
 close_exp_wrt_exp_rec k n1 x1 e1 =
@@ -276,7 +278,7 @@ close x e = bind SZ (close_exp_wrt_exp_rec SZ FZ x e)
 impl :: LambdaImpl
 impl =
   LambdaImpl
-    { impl_name = "LocallyNamelessV2",
+    { impl_name = "LocallyNamelessTyped",
       impl_fromLC = toDB,
       impl_toLC = fromDB,
       impl_nf = nfd,
@@ -284,7 +286,27 @@ impl =
       impl_aeq = aeq
     }
 
-{- ------------------------------------------ -}
+{-
+toDB :: LC.LC IdInt -> DB Z
+toDB = to []
+  where to :: [(IdInt, Idx n)] -> LC IdInt ->  DB n
+        to vs (Var v)   = DVar (fromJust (lookup v vs))
+        to vs (Lam v b) = DLam (bind b') where
+             b' = to ((v,FZ):mapSnd FS vs) b
+        to vs (App f a) = DApp (to vs f) (to vs a)
+-
+
+fromDB :: DB n -> LC IdInt
+fromDB = from firstBoundId
+  where
+    from :: IdInt -> DB n -> LC IdInt
+    from (IdInt n) (DVar i)
+      | toInt i < 0 = Var (IdInt $ toInt i)
+      | toInt i >= n = Var (IdInt $ toInt i)
+      | otherwise = Var (IdInt (n - (toInt i) -1))
+    from n (DLam b) = Lam n (from (succ n) (unbind b))
+    from n (DApp f a) = App (from n f) (from n a)
+-}
 
 toDB :: LC.LC IdInt -> Exp Z
 toDB = to SZ []
@@ -327,6 +349,22 @@ instance NFData (Exp n) where
 weaken :: forall m n. Exp n -> Exp (Plus n m)
 weaken = Unsafe.unsafeCoerce
 
+{-
+weaken (Var_b i) = Var_b (weakenIdx @m i)
+weaken (Var_f x) = Var_f x
+weaken (Abs (Bind ss a)) = Abs (Bind (weakenSubst @m ss) (weaken @m a))
+weaken (App a b) = App (weaken @m a) (weaken @m b)
+
+weakenIdx :: forall m n. Idx n -> Idx (Plus n m)
+weakenIdx FZ = FZ
+weakenIdx (FS n) = FS (weakenIdx @m n)
+
+weakenSubst :: forall p m n. Sub Exp m n -> Sub Exp (Plus m p) (Plus n p)
+weakenSubst (Inc x) = Unsafe.unsafeCoerce $ Inc x
+weakenSubst (Cons a ss) = Cons (weaken @p a) (weakenSubst @p ss)
+weakenSubst (s1 :<> s2) = weakenSubst @p s1 :<> weakenSubst @p s2
+-}
+
 -- free variable substitution
 subst :: Exp Z -> IdInt -> Exp Z -> Exp Z
 subst u y e = subst0 SZ e
@@ -346,7 +384,46 @@ fv e =
     (Abs b) -> fv (unbind b)
     (App e1 e2) -> fv e1 `Set.union` fv e2
 
-{- --------------------------------------- -}
+{-
+open_exp_wrt_exp_rec :: Int -> Exp -> Exp -> Exp
+open_exp_wrt_exp_rec k u e =
+  case e of
+    (Var_b n) ->
+      case compare n k of
+        LT -> Var_b n
+        EQ -> u
+        GT -> Var_b (n - 1)
+    (Var_f x) -> Var_f x
+    (Abs e) -> Abs (openBindRec (k + 1) u e)
+    (App e1 e2) ->
+      App
+        (open_exp_wrt_exp_rec k u e1)
+        (open_exp_wrt_exp_rec k u e2)
+
+open_exp_wrt_map_rec :: IM.IntMap IdInt -> Exp -> Exp
+open_exp_wrt_map_rec k e =
+  case e of
+    (Var_b n) ->
+      case compare n k of
+        LT -> Var_b n
+        EQ -> u
+        GT -> Var_b (n - 1)
+    (Var_f x) -> Var_f x
+    (Abs e) -> Abs (openRec (k + 1) e)
+    -- need to increment the domain of all
+    (App e1 e2) ->
+      App
+        (open_exp_wrt_map_rec k e1)
+        (open_exp_wrt_map_rec k e2)
+
+close_exp_wrt_exp_rec :: Int -> IdInt -> Exp -> Exp
+close_exp_wrt_exp_rec n1 x1 e1 =
+  case e1 of
+    Var_f x2 -> if (x1 == x2) then (Var_b n1) else (Var_f x2)
+    Var_b n2 -> if (n2 < n1) then (Var_b n2) else (Var_b (1 + n2))
+    Abs e2 -> Abs (closeBindRec (1 + n1) x1 e2)
+    App e2 e3 -> App (close_exp_wrt_exp_rec n1 x1 e2) (close_exp_wrt_exp_rec n1 x1 e3)
+-}
 
 type N a = State IdInt a
 
